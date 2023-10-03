8dd0a54e5da00ae594e6e3ed67bc04f24fec0122
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 08:28:18 +0700

    drop `esnext.typed-array.from-async` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2036,24 +2036,6 @@ export const data = {
   'esnext.array-buffer.transfer': null,
   // TODO: Remove from `core-js@4`
   'esnext.array-buffer.transfer-to-fixed-length': null,
-  'esnext.array-buffer.detached': {
-    bun: '1.0.19',
-    chrome: '114',
-    firefox: '122',
-    safari: '17.4',
-  },
-  'esnext.array-buffer.transfer': {
-    bun: '1.0.19',
-    chrome: '114',
-    firefox: '122',
-    safari: '17.4',
-  },
-  'esnext.array-buffer.transfer-to-fixed-length': {
-    bun: '1.0.19',
-    chrome: '114',
-    firefox: '122',
-    safari: '17.4',
-  },
   'esnext.async-disposable-stack.constructor': {
   },
   'esnext.async-iterator.constructor': {
@@ -2412,9 +2394,6 @@ export const data = {
   },
   // TODO: Remove from `core-js@4`
   'esnext.symbol.replace-all': {
-  },
-  // TODO: Remove from `core-js@4`
-  'esnext.typed-array.from-async': {
   },
   'esnext.typed-array.filter-reject': {
   },
