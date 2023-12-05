e2d5a84ce116ea78add355ba23265a84514237da
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-05 23:54:05 +0700

    drop `Math.{ clamp, DEG_PER_RAD, degrees, fscale, RAD_PER_DEG, radians, scale }` (proposal is dead)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2102,22 +2102,8 @@ export const data = {
   },
   'esnext.map.update': {
   },
-  'esnext.math.clamp': {
-  },
-  'esnext.math.deg-per-rad': {
-  },
-  'esnext.math.degrees': {
-  },
-  'esnext.math.fscale': {
-  },
   'esnext.math.f16round': {
     deno: '1.43',
-  },
-  'esnext.math.rad-per-deg': {
-  },
-  'esnext.math.radians': {
-  },
-  'esnext.math.scale': {
   },
   'esnext.math.sum-precise': {
   },
