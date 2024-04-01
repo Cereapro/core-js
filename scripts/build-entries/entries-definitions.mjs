026991f6efb3a2a5108e6d3401c8006e17ca6f52
Denis Pushkarev <zloirock@zloirock.ru>
2024-04-02 03:49:23 +0700

    add a couple of missed entries definitions

--- scripts/build-entries/entries-definitions.mjs
+++ scripts/build-entries/entries-definitions.mjs
@@ -2939,6 +2939,11 @@ export const features = {
     template: $namespace,
     name: 'URL',
   },
+  'url/constructor': {
+    modules: ['web.url.constructor', 'web.url.to-json', /^web\.url-search-params?\./],
+    template: $namespace,
+    name: 'URL',
+  },
   'url/can-parse': {
     modules: ['web.url.can-parse'],
     template: $static,
@@ -2964,6 +2969,11 @@ export const features = {
     name: 'toJSON',
   },
   'url-search-params/index': {
+    modules: [/^web\.url-search-params\./],
+    template: $namespace,
+    name: 'URLSearchParams',
+  },
+  'url-search-params/constructor': {
     modules: [/^web\.url-search-params\./],
     template: $namespace,
     name: 'URLSearchParams',
